8065c63d326781322a5b5c7358e71c047e6e3986
MothNik <nik.zoe@web.de>
2024-11-10 13:49:13 +0100

    ENH: - enabled two-sided cutoff scenarios in `window_with_smooth_cutoff`

--- src/pyscopee/spectra_simulate/window.py
+++ src/pyscopee/spectra_simulate/window.py
@@ -8,6 +8,8 @@ ranges.
 
 # === Imports ===
 
+from typing import Tuple, Union
+
 import numpy as np
 from numpy.typing import NDArray
 
@@ -25,7 +27,7 @@ def window_with_smooth_cutoff(
     x: RealNumericArrayLike,
     x_min: RealNumeric,
     x_max: RealNumeric,
-    exponent: RealNumeric = 20,
+    exponent: Union[RealNumeric, Tuple[RealNumeric, RealNumeric]] = 20,
 ) -> NDArray[np.float64]:
     """
     Computes a smooth window function that is non-zero in the range ``[x_min, x_max]``
@@ -47,10 +49,16 @@ def window_with_smooth_cutoff(
     x_min, x_max : class:`int` or :class:`float`
         The minimum and maximum x-values of the window function beyond which the
         function is zero.
+        The window is centered at ``0.5 * (x_min + x_max)`` where it reaches its
+        maximum value of ``1``.
         Flipped values are automatically corrected, but coinciding values will result
         in an error.
-    exponent : class:`int` or :class:`float`, default=``20``
-        The exponent that controls how sharp the cutoff is.
+    exponent : class:`int` or :class:`float` or (:class:`int` or :class:`float`, :class:`int` or :class:`float`), default=``20``
+        The exponent(s) ``m`` that controls how sharp the cutoff is.
+        Scalars will be used for both the lower and upper cutoff.
+        If provided as a tuple, the first value is used for the lower cutoff and the,
+        i.e., ``x < 0.5 * (x_min + x_max)``, and the second value for the upper cutoff
+        ``x > 0.5 * (x_min + x_max)``.
         A higher value results in a sharper cutoff.
         It has to be a positive value ``>= 1``.
         The default value of ``20`` is already quite close to a step function.
@@ -68,7 +76,7 @@ def window_with_smooth_cutoff(
     ValueError
         If the ``exponent`` is not positive.
 
-    """
+    """  # noqa: E501
 
     # --- Input Validation ---
 
@@ -98,28 +106,41 @@ def window_with_smooth_cutoff(
         x_min, x_max = x_max, x_min
 
     # the exponent is checked and converted to a positive float
-    exponent = get_validated_real_numeric(
-        value=exponent,
-        name="exponent",
-        min_value=1.0,
+    if not isinstance(exponent, tuple):
+        exponent = (exponent, exponent)
+
+    exponent = tuple(  # type: ignore
+        get_validated_real_numeric(
+            value=exp,
+            name=f"exponent [{iter_i}]",
+            min_value=1.0,
+        )
+        for iter_i, exp in enumerate(exponent)
     )
 
     # --- Computation ---
 
     # only the window values for the non-zero x-values are computed
-    x_nonzero_indices = np.logical_and(x >= x_min, x <= x_max)
-    x_zero_indices = np.where(np.logical_not(x_nonzero_indices))[0]
-    x_nonzero_indices = np.where(x_nonzero_indices)[0]
+    nonzero_indices = np.logical_and(x >= x_min, x <= x_max)
+    zero_indices = np.where(np.logical_not(nonzero_indices))[0]
+    nonzero_indices = np.where(nonzero_indices)[0]
 
-    if x_nonzero_indices.size <= 0:
+    if nonzero_indices.size <= 0:
         return np.zeros_like(x)
 
     window = np.empty_like(x)
-    window[x_zero_indices] = 0.0
+    window[zero_indices] = 0.0
 
-    u_values = np.abs(
-        (x[x_nonzero_indices] - 0.5 * (x_min + x_max)) / (0.5 * (x_max - x_min))
-    )
-    window[x_nonzero_indices] = 0.5 * (1 + np.cos(np.pi * (u_values**exponent)))
+    # to distinguish between the lower and upper cutoff, the x-values are normalised
+    # and the indices are split accordingly
+    u_values = (x[nonzero_indices] - 0.5 * (x_min + x_max)) / (0.5 * (x_max - x_min))
+    low_cutoff_indices = np.where(u_values < 0.0)[0]
+    high_cutoff_indices = np.where(u_values >= 0.0)[0]
+    u_values = np.abs(u_values)
+
+    for indices, exp in zip((low_cutoff_indices, high_cutoff_indices), exponent):  # type: ignore
+        window[nonzero_indices[indices]] = 0.5 * (
+            1 + np.cos(np.pi * (u_values[indices] ** exp))
+        )
 
     return window
