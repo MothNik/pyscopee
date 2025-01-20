daba90b242477199b89c9339f5ad36e0a631b4ea
MothNik <nik.zoe@web.de>
2025-01-20 20:18:53 +0100

    DOC: - improved example code for Burg autoregressive extrapolation

--- docs/02_burg_autoregression_single_segment.py
+++ docs/02_burg_autoregression_single_segment.py
@@ -7,22 +7,26 @@ when applied to a single signal.
 # === Imports ===
 
 import os
+from time import perf_counter_ns
 
 import numpy as np
 from matplotlib import pyplot as plt
 
+from pyscopee import apply_pyscopee_plot_style
 from pyscopee.augment import arburg, extrapolate_autoregressive
 
-plt.style.use(os.path.join(os.path.dirname(__file__), "./pyscopee.mplstyle"))
+apply_pyscopee_plot_style()
+
+os.environ["CUSTOM_NUMBA_NO_JIT"] = "0"
 
 # === Constants ===
 
 # the size of the signal
-SIGNAL_SIZE = 1000
+SIGNAL_SIZE = 1_000
 # the order of the autoregressive model
 AR_ORDER = 800
 # the number of samples to extrapolate
-NUM_SAMPLES_EXTRAPOLATED = 2000
+NUM_SAMPLES_EXTRAPOLATED = 1_000
 
 # the path to save the plot
 PLOT_FILEPATH = "./example_plots/02_burg_autoregression_single_segment.png"
@@ -54,20 +58,27 @@ y = (
 )
 
 # the AR coefficients are computed
+start_time = perf_counter_ns()
 y_truncated = y[NUM_SAMPLES_EXTRAPOLATED:-NUM_SAMPLES_EXTRAPOLATED]
 ar_coeffs = arburg(
     xs=y_truncated,
     order=AR_ORDER,
     tikhonov_lambda=1e0,
-    jit=True,
+)
+print(
+    f"AR coefficients computation took {(1e-6 * (perf_counter_ns() - start_time)):.3f} "
+    f"milliseconds."
 )
 
-
 # the signal is extrapolated
+start_time = perf_counter_ns()
 y_extrapolated = extrapolate_autoregressive(
     x=y_truncated,
     ar_coeffs=ar_coeffs,
     pad_width=(NUM_SAMPLES_EXTRAPOLATED, NUM_SAMPLES_EXTRAPOLATED),
+)
+print(
+    f"Extrapolation took {(1e-6 * (perf_counter_ns() - start_time)):.3f} milliseconds."
 )
 
 # the plot is created
@@ -93,4 +104,9 @@ ax.legend()
 ax.set_xlabel("Sample index")
 ax.set_ylabel("Signal value")
 
+# the plot is saved ...
+if os.getenv("pyscopee_DEVELOPER", "false").lower() == "true":
+    plt.savefig(os.path.join(os.path.dirname(__file__), PLOT_FILEPATH))
+
+# ... and shown
 plt.show()
