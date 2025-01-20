0d4679f18916f191e42a2e33db8e9504aa52a269
MothNik <nik.zoe@web.de>
2025-01-20 20:00:26 +0100

    DEV: - cleaned up all scripts and modules that were currently distracting the development by spreading the focus too much - current focus will be on extrapolation

--- src/pyscopee/fts/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Module :mod:`fts`
-
-This module provides functions useful for Fourier transform spectroscopy (FTS), e.g.,
-
-- different apodization functions
-- optimization of apodization functions
-
-"""
