7b7170e31e5071e50d7b1da7fcd46be6690337fe
MothNik <nik.zoe@web.de>
2025-07-14 21:54:55 +0200

    ENH: `_utils` - delete old validation and typing modules

--- src/pyscopee/_utils/__init__.py
+++ src/pyscopee/_utils/__init__.py
@@ -7,20 +7,8 @@ This module provides utilities used across the ``pyscopee`` package.
 
 # === Imports ===
 
-from ._custom_types import Integer, RealNumeric, RealNumericArrayLike  # noqa: F401
 from ._miscellaneous import split_class_name_to_readable, warn_verbose  # noqa: F401
 from ._numba_helpers import jit  # noqa: F401
-from ._validate import (  # noqa: F401
-    NumPyDTypeKinds,
-    get_validated_integer,
-    get_validated_numeric_nd_array_like,
-    get_validated_real_numeric,
-    get_validated_real_numeric_1d_array_like,
-    get_validated_real_numeric_2d_array_like,
-    isinstance_incl_none,
-    validate_1d_array_is_evenly_spaced,
-    validate_1d_array_is_sorted,
-)
 from ._visuals import (  # noqa: F401
     apply_pyscopee_plot_style,
     get_pyscopee_style,
